88ecc3c214d87c1d19ac6dcd054830912d4b434e
Luis <luis.arpasi@cignium.com>
2018-01-30 15:15:45 -0500

    Set Swagger UI as welcome page

--- Belatrix.MoneyExchange.WebApi/App_Start/RouteConfig.cs
+++ Belatrix.MoneyExchange.WebApi/App_Start/RouteConfig.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
+using Swashbuckle.Application;
 
 namespace Belatrix.MoneyExchange.WebApi
 {
@@ -8,6 +10,13 @@ namespace Belatrix.MoneyExchange.WebApi
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapHttpRoute(
+                name: "swagger_root",
+                routeTemplate: "",
+                defaults: null,
+                constraints: null,
+                handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
         }
     }
 }
